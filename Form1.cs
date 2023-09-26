63e5d66e8cb93b1fbe9c7645240485461c3bfde1
Rohabansari <146052852+Rohabansari@users.noreply.github.com>
2023-09-26 09:08:02 +0500

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
-namespace DBTheoryTask
+namespace DBForms
 {
     public partial class Form1 : Form
     {
